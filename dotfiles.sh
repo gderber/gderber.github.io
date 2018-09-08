908abd3c73c61fbbc2d454818653c430d76ecb27
G S Derber <gd.smlinux@gmail.com>
2018-09-07 23:18:47 -0400

    Tweeks

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# ======================================================================
 #
 # dotfiles.sh ---
 # 
@@ -9,9 +10,9 @@
 # Created: Fri Sep  7 15:58:44 2018 (-0400)
 # Version: 
 # Package-Requires: ()
-# Last-Updated: Fri Sep  7 16:05:44 2018 (-0400)
+# Last-Updated: Fri Sep  7 23:17:30 2018 (-0400)
 #           By: Geoff S Derber
-#     Update #: 1
+#     Update #: 7
 # URL: 
 # Doc URL: 
 # Keywords: 
@@ -44,7 +45,7 @@
 # along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 # 
 # 
-
+# ======================================================================
 # Code:
 
 
@@ -88,12 +89,12 @@ HELP
 # ======================================================================
 idghuser () {
     if [ -z $1 ]; then
-	read -p "What is your github username? (Leave blank if you do not have one)" GHUSER
+        read -p "What is your github username? (Leave blank if you do not have one)" GHUSER
     else
-	GHUSER=$1
+        GHUSER=$1
     fi
     if [ -n ${GHUSER} ]; then
-	echo "GHUID=${GHUSER}" > "${DOTFILESRC}"
+        echo "GHUID=${GHUSER}" > "${DOTFILESRC}"
     fi
 }
 
@@ -108,15 +109,15 @@ updateremotes () {
     local GITFQDN=${GITHOST}.${DN}
     unset ORIGIN
     local ORIGIN
-    
+
     if ping -c1 git > /dev/null; then
-	ORIGIN=git
+        ORIGIN=git
     elif ping -c1 ${GITFQDN} > /dev/null; then
-	ORIGIN=${GITFQDN}
+        ORIGIN=${GITFQDN}
     fi
     if [ -n "${ORIGIN}" ] ; then
-	git remote rename origin github
-	git remote add origin "git@${ORIGIN}:dotfiles.git"
+        git remote rename origin github
+        git remote add origin "git@${ORIGIN}:dotfiles.git"
     fi
 }
 
@@ -124,12 +125,14 @@ updateremotes () {
 #
 # function download
 #
+# Clone or Update the dotfiles repo
+#
 # ======================================================================
 download () {
     if [ -d ${DOTFILEDIR} ]; then
         # Update
         cd ${DOTFILEDIR}
-        if ping -c1 git &>/dev/null ; then
+        if ping -c1 git > /dev/null ; then
             git pull origin master
         else
             git pull github master
@@ -172,13 +175,14 @@ selfinstall () {
     echo ${PWD}
 
     if [ ! -f ${PWD}/dotfiles.sh ]; then
-	if [ "${INSTDIR}" != "${PWD}" ]; then
-	    if [ ! -f "${INSTDIR}/dotfiles.sh" ] || [ "${TODAY}" != "${LASTUPDATE}" ]; then
-		mkdir -pv ${INSTDIR} &&
-		    ${SUDO} cp ${DOTFILESSHDIR}/dotfiles.sh ${INSTDIR} &&
-		    chmod 755 ${INSTDIR}/dotfiles.sh
-	    fi	
-	fi
+        if [ "${INSTDIR}" != "${PWD}" ]; then
+            if [ ! -f "${INSTDIR}/dotfiles.sh" ] ||
+                   [ "${TODAY}" != "${LASTUPDATE}" ]; then
+                mkdir -pv ${INSTDIR} &&
+                    ${SUDO} cp ${DOTFILESSHDIR}/dotfiles.sh ${INSTDIR} &&
+                    chmod 755 ${INSTDIR}/dotfiles.sh
+            fi
+        fi
     fi
 }
 
@@ -189,43 +193,43 @@ selfinstall () {
 # ======================================================================
 install () {
     cd ${DOTFILEDIR} &&
-	make install
+        make install
 }
-    
+
 while :; do
     case $1 in
-	--prefix)
-	    if [ -n $2 ]; then
-		PREFIX=$2
-		shift
-	    else
-		printf 'ERROR: "--prefix" requires a non-empty option argument.\n' >&2
-		exit 1
-	    fi
-	    ;;
-	--ghuser)
-	    if [ -n $2 ]; then
-		GHUSER=$2
-	        shift
-	    else
-		printf 'ERROR: "--file" requires a non-empty option argument.\n' >&2
-		exit 1
-	    fi
-	    ;;
-	--reset)
-	    RESET=true
-	    ;;
-	-h|-\?|--help)
-	    dfhelp
-	    exit
-	    ;;
-	-?*)
-	    dfhelp
-	    exit 1
-	    ;;
-	*)
-	    break
-	    ;;
+        --prefix)
+            if [ -n $2 ]; then
+                PREFIX=$2
+                shift
+            else
+                printf 'ERROR: "--prefix" requires a non-empty option argument.\n' >&2
+                exit 1
+            fi
+            ;;
+        --ghuser)
+            if [ -n $2 ]; then
+                GHUSER=$2
+                shift
+            else
+                printf 'ERROR: "--file" requires a non-empty option argument.\n' >&2
+                exit 1
+            fi
+            ;;
+        --reset)
+            RESET=true
+            ;;
+        -h|-\?|--help)
+            dfhelp
+            exit
+            ;;
+        -?*)
+            dfhelp
+            exit 1
+            ;;
+        *)
+            break
+            ;;
     esac
 done
 
@@ -234,22 +238,22 @@ if [ "${RESET}" != "true " ] && [ -f ${DOTFILESRC} ]; then
 else
     idghuser
 fi
-    
+
 download &&
     install &&
     if [ -f ${PWD}/dotfiles.sh ]; then
-	selfinstall ${PREFIX} &&
-	    echo "..."
+        selfinstall ${PREFIX} &&
+            echo "..."
     fi
-    if [ -f ${DOTFILESRC} ]; then
-	if $(grep -q LASTUPDATE ${DOTFILESRC}) ; then
-	    sed -i 's/LASTUPDATE=.*/LASTUPDATE='$(date +%F)'/g' "${DOTFILESRC}"
-	else
-	    echo "LASTUPDATE=$(date +%F)" >> "${DOTFILESRC}"
-	fi
+if [ -f ${DOTFILESRC} ]; then
+    if $(grep -q LASTUPDATE ${DOTFILESRC}) ; then
+        sed -i 's/LASTUPDATE=.*/LASTUPDATE='$(date +%F)'/g' "${DOTFILESRC}"
     else
-	echo "LASTUPDATE=$(date +%F)" > "${DOTFILESRC}"
+        echo "LASTUPDATE=$(date +%F)" >> "${DOTFILESRC}"
     fi
+else
+    echo "LASTUPDATE=$(date +%F)" > "${DOTFILESRC}"
+fi
 
-# 
+#
 # dotfiles.sh ends here
